d5c031747895fc0d78925e028074f3b995739c2a
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-10-16 13:45:59 +0200

    ex11 OK

---
+++ day_01/ex11/do_op_2.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php
+<?PHP
+function	epur_str($str)
+{
+	$str = preg_replace("/ /", "", $str);
+	return ($str);
+}
+
+if ($argc != 2)
+{
+	print("Incorrect Parameters\n");
+	return ;
+}
+$str = epur_str($argv[1]);
+if (preg_match("#[0-9]|\+|\-|\*|\/|%#", $str) == FALSE)
+{
+	print("Syntax Error\n");
+	return (-1);
+}
+$tab = preg_split("#\+|\-|\*|\/|\%#", $str);
+if (count($tab) != 2)
+{
+	print("Syntax Error\n");
+	return (-1);
+}
+if (preg_match("/\+/", $str) == TRUE)
+	echo $tab[0] + $tab[1];
+if (preg_match("/\-/", $str) == TRUE)
+	echo $tab[0] - $tab[1];
+if (preg_match("/\*/", $str) == TRUE)
+	echo $tab[0] * $tab[1];
+if (preg_match("/\//", $str) == TRUE)
+	echo $tab[0] / $tab[1];
+if (preg_match("/\%/", $str) == TRUE)
+	echo $tab[0] % $tab[1];
+echo "\n";
+?>
