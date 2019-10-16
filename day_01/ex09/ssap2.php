62691204c9412e17313d68a9a8d80e2101c3d3ae
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-10-16 11:53:20 +0200

    ex09

---
+++ day_01/ex09/ssap2.php
@@ -0,0 +1,44 @@
+#!/usr/bin/php
+<?PHP
+function	ft_split($str)
+{
+	$array = preg_split("/ +/", $str);
+	return ($array);
+}
+
+$array = array();
+if ($argc < 2)
+	return ;
+foreach ($argv as $elem)
+{
+	if ($elem == $array[0])
+		continue ;
+	$tmp = ft_split($elem);
+	if ($tmp != NULL)
+		$array = array_merge($array, $tmp);
+	else
+		$array = $array.$elem;
+}
+natcasesort($array);
+foreach($array as $elem)
+{
+	if ($elem == $array[0])
+		continue ;
+	if (ctype_alpha($elem) == TRUE)
+		echo "$elem\n";
+}
+foreach($array as $elem)
+{
+	if ($elem == $array[0])
+		continue ;
+	if (is_numeric($elem) == TRUE)
+		echo "$elem\n";
+}
+foreach($array as $elem)
+{
+	if ($elem == $array[0])
+		continue ;
+	if (preg_match("#[^a-zA-Z0-9]#", $elem) == TRUE)
+		echo "$elem\n";
+}
+?>
