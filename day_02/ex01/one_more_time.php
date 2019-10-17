204a7661755f530fa79c0a085f2c9244e0a6620d
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-10-17 14:42:00 +0200

    ex01 OK

--- day_02/ex01/one_more_time.php
+++ day_02/ex01/one_more_time.php
@@ -2,12 +2,68 @@
 <?PHP
 $day = "([Ll]undi|[Mm]ardi|[Mm]ercredi|[Jj]eudi|[Vv]endredi|[Ss]amedi|[Dd]imanche)";
 $day_num = "((0?[1-9])|([0-2][[0-9])|(3[0-1]))";
-$month = "([Jj]anvier|[Ff][ée]vrier|[Mm]ars|[Aa]vril|[Mm]ai|[Jj]uin|[Jj]uillet|[Aa]o[ûu]t|[Ss]eptembre|[Oo]ctobre|[Nn]ovembre|[Dd][ée]cembre)";
+$month = "([Jj]anvier|[Ff]evrier|[Mm]ars|[Aa]vril|[Mm]ai|[Jj]uin|[Jj]uillet|[Aa]out|[Ss]eptembre|[Oo]ctobre|[Nn]ovembre|[Dd]ecembre)";
+$year = "[0-9][0-9][0-9][0-9]";
 $hour = "(([0-1][0-9]|2[0-3])(:[0-5][0-9]){2})";
+
+function	check_day($day, $timestamp)
+{
+	$array = array(
+		"Monday" => "lundi",
+		"Tuesday" => "mardi",
+		"Wednesday" => "mercredi",
+		"Thursday" => "jeudi",
+		"Friday" => "vendredi",
+		"Saturday" => "samedi",
+		"Sunday" => "dimanche"
+	);
+	$ref = date("l", (int)$timestamp);
+	$day = strtolower($day);
+	if ($day != $array[$ref])
+		return (false);
+	return (true);
+}
+
+function	nb_month($str)
+{
+	$array = array(
+		"janvier" => 1,
+		"fevrier" => 2,
+		"mars" => 3,
+		"avril" => 4,
+		"mai" => 5,
+		"juin" => 6,
+		"juillet" => 7,
+		"aout" => 8,
+		"septembre" => 9,
+		"octobre" => 10,
+		"novembre" => 11,
+		"decembre" => 12
+	);
+	$str = strtolower($str);
+	return ($array[$str]);
+}
 
 if ($argc != 2)
 	exit();
 array_shift($argv);
-if (preg_match("/^$day $day_num $month $hour$/", $argv[0]) == 0)
+if (preg_match("/^$day $day_num $month $year $hour$/", $argv[0]) == 0)
+{
 	print("Wrong Format!\n");
+	exit();
+}
+$date = explode(" ", $argv[0]);
+$date[2] = nb_month($date[2]);
+if (checkdate($date[2], (int)$date[1], (int)$date[3]) == false)
+{
+	print("Date doesn't exist");
+	exit();
+}
+$timestamp = strtotime("$date[1]-$date[2]-$date[3] $date[4] +100");
+if (check_day($date[0], $timestamp) == false)
+{
+	print("Date doesn't exist");
+	exit();
+}
+print($timestamp);
 ?>
